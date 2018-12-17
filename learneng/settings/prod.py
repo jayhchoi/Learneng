cc603ce59e5c3ce0a50d952b54d430244bf58ac5
JayHChoi <jaychoi1619@gmail.com>
2018-12-17 18:39:04 +0900

    Static folder move

--- learneng/settings/prod.py
+++ learneng/settings/prod.py
@@ -140,7 +140,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'learneng/static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
