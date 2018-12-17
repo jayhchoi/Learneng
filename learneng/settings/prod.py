e4cfcd81bf81105dd18569e1bc87fe38667b2efb
JayHChoi <jaychoi1619@gmail.com>
2018-12-17 17:32:50 +0900

    Production settings edit

--- learneng/settings/prod.py
+++ learneng/settings/prod.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [config('ALLOWED_HOSTS'), ]
 
 
 # Application definition
@@ -85,8 +85,11 @@ WSGI_APPLICATION = 'learneng.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': 'localhost'
     }
 }
 
