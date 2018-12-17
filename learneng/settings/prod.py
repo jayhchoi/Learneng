88923e682150454eb7f6c54dd50393b7e7beb1c9
JayHChoi <jaychoi1619@gmail.com>
2018-12-17 18:42:10 +0900

    Production setting edit

--- learneng/settings/prod.py
+++ learneng/settings/prod.py
@@ -142,9 +142,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'learneng/static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Media files
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
