99f9ba06f99847868d59d49e561cf7c01266ebb0
JayHChoi <jaychoi1619@gmail.com>
2018-12-26 14:59:40 +0900

    debugging typo

--- accounts/models.py
+++ accounts/models.py
@@ -27,7 +27,7 @@ class Profile(models.Model):
         total_rating = 0
         for comment in comment_qs:
             total_rating += comment.rating
-        if note comment_qs.count():
+        if not comment_qs.count():
             average_rating = total_rating / comment_qs.count()
         else:
             average_rating = 0
