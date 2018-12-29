8ec7e0b8b589ed322155220da7c30c56cb46c466
JayHChoi <jaychoi1619@gmail.com>
2018-12-30 01:33:33 +0900

    comment delete functionality add

--- comments/urls.py
+++ comments/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import comment_create_view
+from .views import comment_create_view, comment_delete_view
 
 
 app_name = 'comments'
 urlpatterns = [
     path('create/', comment_create_view, name='create'),
+    path('<int:pk>/delete/', comment_delete_view, name='delete'),
 ]
--- comments/views.py
+++ comments/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, get_object_or_404
 from django.http import HttpResponseRedirect, Http404, HttpResponse
 from django.contrib.auth import get_user_model
 from django.contrib import messages
+from django.views.generic import DeleteView
 
 from .models import Comment
 from .forms import CommentForm
@@ -18,6 +19,13 @@ def comment_create_view(request):
             return HttpResponseRedirect(next)
 
         leader_id = request.POST.get('leader_id')
+        leader_obj = get_object_or_404(User, id=leader_id)
+
+        # Only one comment per leader is allowed
+        if Comment.objects.filter(member=request.user, leader=leader_obj).exists():
+            messages.warning(request, "이미 작성한 리뷰가 있습니다.")
+            return HttpResponseRedirect(next)
+        
         form = CommentForm(request.POST)
         if form.is_valid():
             form.instance.member = request.user
@@ -28,3 +36,15 @@ def comment_create_view(request):
             
         return HttpResponseRedirect(next)
     raise Http404
+
+
+def comment_delete_view(request, pk):
+    if request.method == 'POST':
+        previous_page = request.POST.get('group_url', '/')
+        comment = get_object_or_404(Comment, pk=pk, member=request.user)
+        comment.delete()
+        return redirect(previous_page)
+    raise Http404
+    
+
+    
